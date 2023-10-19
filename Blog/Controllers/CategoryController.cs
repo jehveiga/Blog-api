0aad8614b7ba6a7b499ca9b16d016b6dc70b731f
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-10-18 21:31:48 -0300

    Adicionar CategoryController e end-ppoint que traz a lista de Categorias cadastradas.

---
+++ Blog/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class CategoryController : ControllerBase
+    {
+        // Adicionando end-points usando a nomeclatura API Rest
+        [HttpGet("categories")]
+        public IActionResult Get(
+            [FromServices] BlogDataContext context)
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}
