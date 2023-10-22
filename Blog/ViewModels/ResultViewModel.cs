e42da07b24b4edcb650507121746df955ded4137
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-10-22 09:30:29 -0300

    Adicionar classe genérica que vai conter os resultados das requisições da API

---
+++ Blog/ViewModels/ResultViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Blog.ViewModels
+{
+    // Classe genérica para informar o resultado da requisição da API para ter uma padronização
+    public class ResultViewModel<T>
+    {
+        public ResultViewModel(T data, List<string> errors) // Recebe os dados e a listagem contendo os erros da requisição
+        {
+            Data = data;
+            Errors = errors;
+        }
+
+        public ResultViewModel(T data) // Recebe os dados no caso de sucesso
+        {
+            Data = data;
+        }
+
+        public ResultViewModel(List<string> errors) // Recebe a listagem contendo os erros da requisição
+        {
+            Errors = errors;
+        }
+
+        public ResultViewModel(string erro) // Recebe um erro contendo o erro da requisição e adicionar a listagem de erros
+        {
+            Errors.Add(erro);
+        }
+
+        public T Data { get; private set; }
+        public List<string> Errors { get; private set; } = new();
+
+    }
+}
