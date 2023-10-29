8728cd3b62d1184417431a3669d502feb72a468c
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-10-29 12:11:28 -0300

    Adicionar e configurar a autenticação e autorização na classe Program

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,10 +1,32 @@
+using Blog;
 using Blog.Data;
 using Blog.Services;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey); // Obtendo a chave JWT configurada
+
 // Add services to the container.
+
+// Confirando o esquema que será usado na autenticação do usuário do sistema
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // Configuração do esquema da autenticação
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // Configuração do desafio da autenticação
+}).AddJwtBearer(x => // Informando o tipo usado da autenticação como será lidado com o token obtido
+{
+    x.TokenValidationParameters = new TokenValidationParameters // Configurando como será encriptado e desecriptado o token e etc
+    {
+        ValidateIssuerSigningKey = true, // Validar a chave da assinatura
+        IssuerSigningKey = new SymmetricSecurityKey(key), // Como será validado a chave desta assinatura
+        ValidateIssuer = false, // Se usar o serviço de multiplas API
+        ValidateAudience = false, // Se usar o serviço de multiplas API
+    };
+
+});
 
 builder.Services
        .AddControllers()
