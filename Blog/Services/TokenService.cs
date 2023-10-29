63a2502394e85a63e10047f833ab2e677364acde
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-10-29 15:24:30 -0300

    Adicionar a chamada de obter os claims vinculados ao usuário alvo para criação e configuração do token

--- Blog/Services/TokenService.cs
+++ Blog/Services/TokenService.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Models;
+﻿using Blog.Extensions;
+using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -13,16 +14,11 @@ namespace Blog.Services
             var tokenHandler = new JwtSecurityTokenHandler(); // Objeto que fará a manipulação do token
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);   // Obtendo a chave da classe estatica criada de Configuration e convertendo para um array de bytes,
                                                                        // a chave informada será usada para encriptar e decriptar.
+            var claims = user.GetClaims();
 
             var tokenDescriptor = new SecurityTokenDescriptor // Objeto que ficará todos as informações vinculado ao token
             {
-                Subject = new ClaimsIdentity(new Claim[] // Criando as claims que serão usadas para validar o usuário 
-                {
-                    new (ClaimTypes.Name, "testetestado"), // User.Identity.Name
-                    new (ClaimTypes.Role, "user"), // User.IsInRole
-                    new (ClaimTypes.Role, "admin"), // User.IsInRole
-                    new ("fruta" , "banana")
-                }),
+                Subject = new ClaimsIdentity(claims), // Adicionar uma lista de Claims vinculado ao usuário para ser passado para configuração de criação do token
                 Expires = DateTime.UtcNow.AddHours(8),
                 SigningCredentials = new SigningCredentials( // Criará o token encriptado passando a chave e o algoritmo que será usado
                     new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
