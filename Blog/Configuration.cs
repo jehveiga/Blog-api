c7e9738ae211fc1a5f68a130ed8670989233c254
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-10-29 10:16:04 -0300

    Criar a classe estica que irá conter o token, criar a classe responsável por gerar o token.

---
+++ Blog/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog;
+
+public static class Configuration
+{
+    // TOKEN - JWT - Json Web Token
+    public static string JwtKey { get; set; } = "DHdXXugZKOQvHFgGnpclTfNJoaoVwdOGUNig";
+}
+
---
+++ Blog/Services/TokenService.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Blog.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler(); // Objeto que fará a manipulação do token
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);   // Obtendo a chave da classe estatica criada de Configuration e convertendo para um array de bytes,
+                                                                       // a chave informada será usada para encriptar e decriptar.
+
+            var tokenDescriptor = new SecurityTokenDescriptor // Objeto que ficará todos as informações vinculado ao token
+            {
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials( // Criará o token encriptado passando a chave e o algoritmo que será usado
+                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor); // Criando um novo token baseado nas informações passadas no objeto acima
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
