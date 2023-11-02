faae166223814bebdf9b3bc9197541d6a1d05ab8
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-11-02 14:11:07 -0300

    Criar classe de configuração SmtpConfiguration

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -7,5 +7,14 @@ public static class Configuration
 
     public static string ApiKeyName = "api_Key";
     public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
 }
 
