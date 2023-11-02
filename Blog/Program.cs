14bd92c4a219cb52c4f0da6288dceca73795455b
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-11-02 14:32:10 -0300

    Adicionar servi;co de configuração a classe Program e organizar a classe program

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -7,36 +7,10 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey); // Obtendo a chave JWT configurada
-
-// Add services to the container.
-
-// Confirando o esquema que será usado na autenticação do usuário do sistema
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // Configuração do esquema da autenticação
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // Configuração do desafio da autenticação
-}).AddJwtBearer(x => // Informando o tipo usado da autenticação como será lidado com o token obtido
-{
-    x.TokenValidationParameters = new TokenValidationParameters // Configurando como será encriptado e desecriptado o token e etc
-    {
-        ValidateIssuerSigningKey = true, // Validar a chave da assinatura
-        IssuerSigningKey = new SymmetricSecurityKey(key), // Como será validado a chave desta assinatura
-        ValidateIssuer = false, // Se usar o serviço de multiplas API
-        ValidateAudience = false, // Se usar o serviço de multiplas API
-    };
-
-});
-
-builder.Services
-       .AddControllers()
-       .ConfigureApiBehaviorOptions(options =>
-       {
-           options.SuppressModelStateInvalidFilter = true; // Definir que o comportamento de validação dos modelos das requições não será feito automático pelos Asp.Net
-       });
-
-builder.Services.AddDbContext<BlogDataContext>(); // adicionando o serviço de injeção de dependência do Contexto que representará o banco na aplicação
-builder.Services.AddTransient<TokenService>();
+// Add services to the container
+ConfigureAuthentication(builder);
+ConfigureMvc(builder);
+ConfigureServices(builder);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -44,6 +18,7 @@ builder.Services.AddSwaggerGen();
 // Add service Data Base Context
 
 var app = builder.Build();
+LoadConfiguration(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -60,3 +35,54 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void LoadConfiguration(WebApplication app)
+{
+    // Obtendo as informações da configuração da aplicação do appsettings.json
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
+    // Obtendo as informações da configuração da aplicação do appsettings.json do serviço de Smtp
+    var smtp = new Configuration.SmtpConfiguration();
+    app.Configuration.GetSection("Smtp").Bind(smtp); // Efetua o mapeamento dos valores da seção para o objeto criado da classe do Smtp
+    Configuration.Smtp = smtp;
+}
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey); // Obtendo a chave JWT configurada
+
+    // Confirando o esquema que será usado na autenticação do usuário do sistema
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // Configuração do esquema da autenticação
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // Configuração do desafio da autenticação
+    }).AddJwtBearer(x => // Informando o tipo usado da autenticação como será lidado com o token obtido
+    {
+        x.TokenValidationParameters = new TokenValidationParameters // Configurando como será encriptado e desecriptado o token e etc
+        {
+            ValidateIssuerSigningKey = true, // Validar a chave da assinatura
+            IssuerSigningKey = new SymmetricSecurityKey(key), // Como será validado a chave desta assinatura
+            ValidateIssuer = false, // Se usar o serviço de multiplas API
+            ValidateAudience = false, // Se usar o serviço de multiplas API
+        };
+
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    builder.Services
+       .AddControllers()
+       .ConfigureApiBehaviorOptions(options =>
+       {
+           options.SuppressModelStateInvalidFilter = true; // Definir que o comportamento de validação dos modelos das requições não será feito automático pelos Asp.Net
+       });
+}
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<BlogDataContext>(); // adicionando o serviço de injeção de dependência do Contexto que representará o banco na aplicação
+    builder.Services.AddTransient<TokenService>();
+}
