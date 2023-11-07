c49b0cc2c03f06603e39018d45e60fdd4bfebdcf
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-11-07 20:39:52 -0300

    Adicionar Método Details a PostController

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -5,54 +5,83 @@ using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace Blog.Controllers
+namespace Blog.Controllers;
+
+[ApiController]
+public class PostController : ControllerBase
 {
-    [ApiController]
-    public class PostController : ControllerBase
+    [HttpGet("v1/posts")]
+    public async Task<IActionResult> GetAsync(
+        [FromServices] BlogDataContext context,
+        [FromQuery] int page = 0, // página que está, que pode ser enviado pela Url (chave = valor)
+        [FromQuery] int pageSize = 25) // quantidade de itens por página, que pode ser enviado pela Url (chave = valor)
     {
-        [HttpGet("v1/posts")]
-        public async Task<IActionResult> GetAsync(
-            [FromServices] BlogDataContext context,
-            [FromQuery] int page = 0, // página que está, que pode ser enviado pela Url (chave = valor)
-            [FromQuery] int pageSize = 25) // quantidade de itens por página, que pode ser enviado pela Url (chave = valor)
+        try
         {
-            try
-            {
-                // Se precisar passar ao frontend a quantidade de registros para calcular o count
-                var count = await context.Posts.AsNoTracking().CountAsync();
+            // Se precisar passar ao frontend a quantidade de registros para calcular o count
+            var count = await context.Posts.AsNoTracking().CountAsync();
 
-                var posts = await context
-                    .Posts
-                    .AsNoTracking()
-                    .Include(post => post.Category)
-                    .Include(post => post.Author)
-                    .Select(post => new ListPostsViewModel
-                    {
-                        Id = post.Id,
-                        Title = post.Title,
-                        Slug = post.Slug,
-                        LastUpdateDate = post.LastUpdateDate,
-                        Category = post.Category.Name,
-                        Author = $"{post.Author.Name} ({post.Author.Email})"
-                    })
-                    .Skip(page * pageSize) // Quanto que será pulado de dados
-                    .Take(pageSize) // Quanto que será obtido de dados
-                    .OrderByDescending(post => post.LastUpdateDate)
-                    .ToListAsync();
-                return Ok(new ResultViewModel<dynamic>(new // Retornando um objeto dinamico para ser passado ao result pode ser criado uma ViewModel
+            var posts = await context
+                .Posts
+                .AsNoTracking()
+                .Include(post => post.Category)
+                .Include(post => post.Author)
+                .Select(post => new ListPostsViewModel
                 {
-                    total = count,
-                    page,
-                    pageSize,
-                    posts
-                }));
-            }
-            catch (Exception)
+                    Id = post.Id,
+                    Title = post.Title,
+                    Slug = post.Slug,
+                    LastUpdateDate = post.LastUpdateDate,
+                    Category = post.Category.Name,
+                    Author = $"{post.Author.Name} ({post.Author.Email})"
+                })
+                .Skip(page * pageSize) // Quanto que será pulado de dados
+                .Take(pageSize) // Quanto que será obtido de dados
+                .OrderByDescending(post => post.LastUpdateDate)
+                .ToListAsync();
+            return Ok(new ResultViewModel<dynamic>(new // Retornando um objeto dinamico para ser passado ao result pode ser criado uma ViewModel
             {
-
-                return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Falha interna do servidor"));
-            }
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
         }
+        catch (Exception)
+        {
 
+            return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Falha interna do servidor"));
+        }
+    }
+
+    [HttpGet("v1/posts/{id:int}")]
+    public async Task<IActionResult> DetailsAsync(
+    [FromServices] BlogDataContext context,
+    [FromRoute] int id)
+    {
+        try
+        {
+            var posts = await context
+                .Posts
+                .AsNoTracking()
+                .Include(post => post.Author)
+                .ThenInclude(post => post.Roles)
+                .Include(post => post.Category)
+                .FirstOrDefaultAsync(post => post.Id == id);
+
+            if (posts == null)
+                return NotFound(new ResultViewModel<Post>("Conteúdo não encontrado"));
+
+            return Ok(new ResultViewModel<Post>(posts));
+
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Falha interna do servidor"));
+        }
     }
 }
+
+
+
