73cfb860b29f8b951d22d8c0c968998af49e20ab
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-11-07 20:27:38 -0300

    Adicionar Paginação na PostController

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -1,4 +1,6 @@
 ﻿using Blog.Data;
+using Blog.Models;
+using Blog.ViewModels;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,24 +12,46 @@ namespace Blog.Controllers
     {
         [HttpGet("v1/posts")]
         public async Task<IActionResult> GetAsync(
-            [FromServices] BlogDataContext context)
+            [FromServices] BlogDataContext context,
+            [FromQuery] int page = 0, // página que está, que pode ser enviado pela Url (chave = valor)
+            [FromQuery] int pageSize = 25) // quantidade de itens por página, que pode ser enviado pela Url (chave = valor)
         {
-            var posts = await context
-                .Posts
-                .AsNoTracking()
-                .Include(post => post.Category)
-                .Include(post => post.Author)
-                .Select(post => new ListPostsViewModel
+            try
+            {
+                // Se precisar passar ao frontend a quantidade de registros para calcular o count
+                var count = await context.Posts.AsNoTracking().CountAsync();
+
+                var posts = await context
+                    .Posts
+                    .AsNoTracking()
+                    .Include(post => post.Category)
+                    .Include(post => post.Author)
+                    .Select(post => new ListPostsViewModel
+                    {
+                        Id = post.Id,
+                        Title = post.Title,
+                        Slug = post.Slug,
+                        LastUpdateDate = post.LastUpdateDate,
+                        Category = post.Category.Name,
+                        Author = $"{post.Author.Name} ({post.Author.Email})"
+                    })
+                    .Skip(page * pageSize) // Quanto que será pulado de dados
+                    .Take(pageSize) // Quanto que será obtido de dados
+                    .OrderByDescending(post => post.LastUpdateDate)
+                    .ToListAsync();
+                return Ok(new ResultViewModel<dynamic>(new // Retornando um objeto dinamico para ser passado ao result pode ser criado uma ViewModel
                 {
-                    Id = post.Id,
-                    Title = post.Title,
-                    Slug = post.Slug,
-                    LastUpdateDate = post.LastUpdateDate,
-                    Category = post.Category.Name,
-                    Author = $"{post.Author.Name} ({post.Author.Email})"
-                })
-                .ToListAsync();
-            return Ok(posts);
+                    total = count,
+                    page,
+                    pageSize,
+                    posts
+                }));
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Falha interna do servidor"));
+            }
         }
 
     }
